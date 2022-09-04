c02a64fe714754d202a3828f6859399fe99e3df8
Arslan <anderfell2003@mail.ru>
2022-09-04 10:49:17 +0500

    Main commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿
+// Задача 27
+
+// Console.WriteLine("Введите число");
+// int N = Convert.ToInt32(Console.ReadLine());
+
+// int sum = 0;
+// for (int i = 0; i <= N; i++)
+// {
+//     sum += i;
+// }
+
+// Console.WriteLine($"Сумма чисел от 0 до {N} = {sum}" );
+
+
+// Задача 29
+
+// int[] GetBinaryArray (int size)
+// {
+//     int[] array = new int [size];
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = new Random().Next();
+//     }
+//     return array;
+// }
+// int sizeArray = 8;
+// int[] resultArray = GetBinaryArray(sizeArray);
+// Console.WriteLine($"Вывод массива:  [ {String.Join ("; ", resultArray)} ] ");
